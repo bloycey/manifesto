c46e05f90d91e1428d87e31adce4c7dcb622db0c
Andrew <bloyce1990@gmail.com>
2019-10-26 06:43:18 +1000

    Link shortening

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,20 +12,48 @@ class Home extends Component {
 		linkToManifesto: null
 	}
 
+	getShortUrl = async (longUrl) => {
+		const url = `https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=${process.env.REACT_APP_FIREBASE_API}`;
+		const data = {
+			"longDynamicLink": longUrl,
+			"suffix": {
+			  "option": "SHORT"
+			}
+		};
+
+		try {
+			const response = await fetch(url, {
+				method: 'POST', // or 'PUT'
+				body: JSON.stringify(data), // data can be `string` or {object}!
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			const json = await response.json();
+			console.log('Success:', JSON.stringify(json));
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	}
+
 	handleSubmit = e => {
 		e.preventDefault();
 		const title = compress(this.titleInput.current.value);
 		const text = compress(this.textInput.current.value);
 		const linkToManifesto = `${window.location.hostname}:3000/${title}__${text}`
+		this.getShortUrl(linkToManifesto);
 		this.setState({
 			linkToManifesto
 		})
 	}
 
+
+
 	render() {
 		const { linkToManifesto } = this.state;
 		return (
 			<div>
+				<p>API key is {process.env.REACT_APP_FIREBASE_API}</p>
 				<form onSubmit={this.handleSubmit}>
 					<label>Title</label>
 					<input type="text" ref={this.titleInput}/>
